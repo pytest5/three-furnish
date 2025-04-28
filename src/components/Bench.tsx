c74e7a2c7e15232e9a62e1e255dbce8c9acd3e4d
pytest5 <pytest514@gmail.com>
2025-04-28 22:44:50 +0800

    fix: correct file import path

--- src/components/Bench.tsx
+++ src/components/Bench.tsx
@@ -2,7 +2,7 @@ import { useGLTF } from "@react-three/drei";
 import { FurnitureWrapper } from "../utils/furniture";
 
 export const Bench = ({ ...props }) => {
-  const gltf = useGLTF("./bench.gltf");
+  const gltf = useGLTF("./bench-noback.gltf");
   return (
     <FurnitureWrapper>
       <primitive {...props} object={gltf.scene} />
