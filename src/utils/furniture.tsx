f551704eb1f38011a5d6c18ae2f4bbaa7f777fb1
pytest5 <pytest514@gmail.com>
2025-04-28 20:35:53 +0800

    fix: anchor pivot control to wrapped element

--- src/utils/furniture.tsx
+++ src/utils/furniture.tsx
@@ -1,5 +1,12 @@
 import { PivotControls } from "@react-three/drei";
 
 export const FurnitureWrapper = ({ scale = 1, lineWidth = 3, ...props }) => {
-  return <PivotControls {...props} scale={scale} lineWidth={lineWidth} />;
+  return (
+    <PivotControls
+      {...props}
+      anchor={[1, 1, 0]}
+      scale={scale}
+      lineWidth={lineWidth}
+    />
+  );
 };
