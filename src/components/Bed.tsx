9b6d8a3f25b665b3b985bf58af96cef8618efc32
pytest5 <pytest514@gmail.com>
2025-04-29 21:17:58 +0800

    chore: remove unused code

--- src/components/Bed.tsx
+++ src/components/Bed.tsx
@@ -1,12 +1,11 @@
 import { useGLTF } from "@react-three/drei";
 import { FurnitureWrapper } from "../utils/furniture";
-import React from "react";
+import { SuspenseWrapper } from "./SuspenseWrapper";
 
 export const Bed = ({ ...props }) => {
   const gltf = useGLTF("./bed.gltf");
-  const furnitureRef = React.useRef(null);
   return (
-    <FurnitureWrapper ref={furnitureRef}>
+    <FurnitureWrapper>
       <primitive {...props} object={gltf.scene} />
     </FurnitureWrapper>
   );
