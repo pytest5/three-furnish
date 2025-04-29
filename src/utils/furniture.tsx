29ac1dd374bef4fb8ba43edd047a5dfc5fde84af
pytest5 <pytest514@gmail.com>
2025-04-29 21:16:25 +0800

    feat add leva gui to toggle pivotcontrols

--- src/utils/furniture.tsx
+++ src/utils/furniture.tsx
@@ -1,25 +1,34 @@
 import { PivotControls } from "@react-three/drei";
+import { useControls } from "leva";
+import React from "react";
 
 export const FurnitureWrapper = ({
   anchor = [0, 0, 0],
   scale = 0.8,
   lineWidth = 3,
-  ...props
+  children,
 }: {
   anchor?: [number, number, number];
   scale?: number;
   lineWidth?: number;
   children: React.ReactNode;
 }) => {
+  const { pivotControls } = useControls({
+    pivotControls: { options: ["False", "True"] },
+  });
+
+  const enabled = pivotControls === "True";
+
   return (
     <PivotControls
-      {...props}
       anchor={anchor}
       scale={scale}
       lineWidth={lineWidth}
       depthTest={false}
       axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
-      // fixed={true}
-    />
+      enabled={enabled}
+    >
+      {children}
+    </PivotControls>
   );
 };
